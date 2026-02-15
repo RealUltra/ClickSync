d77d4c67578a2666dc69f703a7dc9579b0e25944
RealUltra <mirzarameezahmedbaig@gmail.com>
2026-02-14 17:02:28 -0800

    added chrome extension scaffolding; improved project structure; added icons

---
+++ docs/DEVLOG.md
@@ -0,0 +1,7 @@
+## 2026-02-14 (1)
+
+**Goal:** Set up all the scaffolding & boilerplate for the chrome extension & react UI.
+
+**Outcome:** Most of the scaffolding is done. Some boilerplate must be written in the service worker & content scripts. Icons added.
+
+**Next Steps:** Write the boilerplate, start planning on what implementation to start with.
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "node:path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, "index.html"),
+        "service-worker": resolve(
+          __dirname,
+          "src/background/service-worker.ts",
+        ),
+        "content-script": resolve(__dirname, "src/content/content-script.ts"),
+      },
+      output: {
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+});
